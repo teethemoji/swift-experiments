b6fa3777fedfe717e09a123e975bf3c1f7ee2249
teethemoji <60784937+teethemoji@users.noreply.github.com>
2022-03-19 19:44:14 +0900

    Create SwiftUI tutorial project
    - https://www.youtube.com/watch?v=F2ojC6TNwws&t=1801s

---
+++ SwiftUICodeWithChris/uitest/uitest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  uitest
+//
+//  Created by 김보영 on 2022/03/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICodeWithChris/uitest/uitest/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by 김보영 on 2022/03/19.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
