adfab3021212e47c2b065fbfabfdc7b27b4ea481
teethemoji <60784937+teethemoji@users.noreply.github.com>
2022-07-01 15:37:15 +0900

    Create Task Management Project

---
+++ Kavsoft/TaskManagementApp/TaskManagementApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskManagementApp
+//
+//  Created by 김보영 on 2022/07/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kavsoft/TaskManagementApp/TaskManagementApp/TaskManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementAppApp.swift
+//  TaskManagementApp
+//
+//  Created by 김보영 on 2022/07/01.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
