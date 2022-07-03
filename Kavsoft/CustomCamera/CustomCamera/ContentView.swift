41a2b7314db5ea042738e18e3ba15242afa11217
teethemoji <60784937+teethemoji@users.noreply.github.com>
2022-07-03 12:37:07 +0900

    Create Custom Camera Project

---
+++ Kavsoft/CustomCamera/CustomCamera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomCamera
+//
+//  Created by 김보영 on 2022/07/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kavsoft/CustomCamera/CustomCamera/CustomCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCameraApp.swift
+//  CustomCamera
+//
+//  Created by 김보영 on 2022/07/03.
+//
+
+import SwiftUI
+
+@main
+struct CustomCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
