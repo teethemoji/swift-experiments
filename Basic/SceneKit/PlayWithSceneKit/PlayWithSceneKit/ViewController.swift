e6c357fc243841301f87b1cc60037aafcef23366
teethemoji <60784937+teethemoji@users.noreply.github.com>
2022-07-16 21:57:10 +0900

    Change cube color with buttons

--- Basic/SceneKit/PlayWithSceneKit/PlayWithSceneKit/ViewController.swift
+++ Basic/SceneKit/PlayWithSceneKit/PlayWithSceneKit/ViewController.swift
@@ -8,100 +8,170 @@
 import UIKit
 import SceneKit
 
+var selectedColor = UIColor.systemBlue
+
 class ViewController: UIViewController {
     
-    static var selectedColor = UIColor.white
+    var leftCubeNode = createCubeNode()
+    var rightCubeNode = createCubeNode()
     
     @IBOutlet weak var sceneView: SCNView!
-
+    
+    @IBAction func redButton(_ sender: UIButton) {
+        onTapButton(buttonIndex: 1)
+        DebugCheckIfButtonWorks()
+    }
+    
+    @IBAction func blueButton(_ sender: UIButton) {
+        onTapButton(buttonIndex: 2)
+        DebugCheckIfButtonWorks()
+    }
+    
+    @IBAction func greenButton(_ sender: UIButton) {
+        onTapButton(buttonIndex: 3)
+        DebugCheckIfButtonWorks()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //        LoadSceneWithCube(at: sceneView)
         
-        LoadSceneWithCube(at: sceneView)
+        // MARK: - Load Scene
+        // 1. Load .obj file
+        let scene = SCNScene(named: "blue_cube.obj")
+        
+        scene?.rootNode.addChildNode(self.leftCubeNode)
+        
+        scene?.rootNode.addChildNode(self.rightCubeNode)
+        
+        self.leftCubeNode.position = SCNVector3(x: -2, y: 0, z: 0)
+        self.rightCubeNode.position = SCNVector3(x: 2, y: 0, z: 0)
+        
+        // 2. Add camera node
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.camera?.fieldOfView = 60.0
+        
+        // 3. Place camera
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 14)
+        
+        // 4. Set camera on scene
+        scene?.rootNode.addChildNode(cameraNode)
+        
+        // MARK: - Set Environment
+        
+        // 5. Add light
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light?.type = .omni
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 35)
+        scene?.rootNode.addChildNode(lightNode)
+        
+        // 6. Add ambient light
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light?.type = .ambient
+        ambientLightNode.light?.color = UIColor.darkGray
+        scene?.rootNode.addChildNode(ambientLightNode)
+        
+        // 배경색 지정
+        sceneView.backgroundColor = UIColor.white
+        
+        // 7. Allow user to manipulate camera
+        sceneView.allowsCameraControl = true
+        
+        // 8. Show statistics
+        sceneView.showsStatistics = true
+                
+        // Set scene settings
+        sceneView.scene = scene
         
     }
     
     func onTapButton(buttonIndex: Int) {
-        // selectedColor의 값을 변경한다
         switch buttonIndex {
-        case 1: ViewController.selectedColor = UIColor.systemRed
+        case 1: self.leftCubeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.systemRed
             break
-        case 2: ViewController.selectedColor = UIColor.systemBlue
+        case 2: self.leftCubeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.systemBlue
             break
-        case 3: ViewController.selectedColor = UIColor.systemGreen
-        default: ViewController.selectedColor = UIColor.white
+        case 3: self.leftCubeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.systemGreen
+            break
+        default: self.leftCubeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.white
+            break
         }
     }
-
-
+    
+    // 굳이 함수로 만들 필요가 없는 듯
+    func LoadSceneWithCube(at: SCNView) {
+        
+        // 1. Load .obj file
+        let scene = SCNScene(named: "blue_cube.obj")
+        // 이름은 블루 큐브인데 막상 material은 적용이 되어 있지 않아서 그레이 큐브인 블루 큐브
+        
+        let leftCubeNode = createCubeNode()
+        scene?.rootNode.addChildNode(leftCubeNode)
+        
+        let rightCubeNode = createCubeNode()
+        scene?.rootNode.addChildNode(rightCubeNode)
+        
+        leftCubeNode.position = SCNVector3(x: -2, y: 0, z: 0)
+        rightCubeNode.position = SCNVector3(x: 2, y: 0, z: 0)
+        
+        // 2. Add camera node
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.camera?.fieldOfView = 60.0
+        
+        // 3. Place camera
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 14)
+        
+        // 4. Set camera on scene
+        scene?.rootNode.addChildNode(cameraNode)
+        
+        // 5. Add light
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light?.type = .omni
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 35)
+        scene?.rootNode.addChildNode(lightNode)
+        
+        // 6. Add ambient light
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light?.type = .ambient
+        ambientLightNode.light?.color = UIColor.darkGray
+        scene?.rootNode.addChildNode(ambientLightNode)
+        
+        // 7. Allow user to manipulate camera
+        at.allowsCameraControl = true
+        
+        // 8. Show statistics
+        at.showsStatistics = true
+        
+        at.backgroundColor = UIColor.white
+        
+        // Set scene settings
+        at.scene = scene
+    }
+    
+    func DebugCheckIfButtonWorks() {
+        print("Button has been clicked")
+    }
 }
 
-func LoadSceneWithCube(at: SCNView) {
+func createCubeNode() -> SCNNode {
     
-    // 1. Load .obj file
-    let scene = SCNScene(named: "blue_cube.obj")
-    
-    // Cube Geometry 생성
-    var cube: SCNGeometry? = SCNBox(width: 2.0, height: 2.0, length: 2.0, chamferRadius: 0)
-    var cubeNode = SCNNode(geometry: cube)
+    let cube: SCNGeometry? = SCNBox(width: 2.0, height: 2.0, length: 2.0, chamferRadius: 0)
     
     // Material 생성
-    var cubeMaterial = SCNMaterial()
+    let cubeMaterial = SCNMaterial()
     cubeMaterial.isDoubleSided = false
-    cubeMaterial.diffuse.contents = ViewController.selectedColor
+    cubeMaterial.diffuse.contents = selectedColor
+    
+    let cubeNode = SCNNode(geometry: cube)
     
     // 생성한 Geometry와 Material 붙여주기
     cubeNode.geometry?.materials = [cubeMaterial]
     
-    scene?.rootNode.addChildNode(cubeNode)
-    
-    cubeNode.position = SCNVector3(x: 2, y: 0, z: 0)
-    
-    /*
-    var imageMaterial = SCNMaterial()
-    imageMaterial.isDoubleSided = false
-    imageMaterial.diffuse.contents = UIImage(named: "myImage")
-    var cube: SCNGeometry? = SCNBox(width: 1.0, height: 1.0, length: 1, chamferRadius: 0)
-    var node = SCNNode(geometry: cube)
-    node.geometry?.materials = [imageMaterial]
-    */
-    
-    // 2. Add camera node
-    let cameraNode = SCNNode()
-    cameraNode.camera = SCNCamera()
-    cameraNode.camera?.fieldOfView = 60.0
-    
-    // 3. Place camera
-    cameraNode.position = SCNVector3(x: 0, y: 0, z: 14)
-    
-    // 4. Set camera on scene
-    scene?.rootNode.addChildNode(cameraNode)
-    
-    // 5. Add light
-    let lightNode = SCNNode()
-    lightNode.light = SCNLight()
-    lightNode.light?.type = .omni
-    lightNode.position = SCNVector3(x: 0, y: 10, z: 35)
-    scene?.rootNode.addChildNode(lightNode)
-    
-    // 6. Add ambient light
-    let ambientLightNode = SCNNode()
-    ambientLightNode.light = SCNLight()
-    ambientLightNode.light?.type = .ambient
-    ambientLightNode.light?.color = UIColor.darkGray
-    scene?.rootNode.addChildNode(ambientLightNode)
-
-    // 7. Allow user to manipulate camera
-    at.allowsCameraControl = true
-    
-    // 8. Show statistics
-    at.showsStatistics = true
-    
-    // 배경색 지정
-    at.backgroundColor = UIColor.white
-    
-    // Set scene settings
-    at.scene = scene
+    return cubeNode
 }
-
